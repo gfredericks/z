14941b6d0d1ecd3185f9688e5ad9ce2b4b9dadf7
Gary Fredericks <fredericksgary@gmail.com>
2014-03-06 20:40:26 -0600

    lein-release plugin: preparing 0.1.0 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.gfredericks/z "0.1.0-SNAPSHOT"
+(defproject com.gfredericks/z "0.1.0"
   :description "Complex numbers in Clojure"
   :url "https://github.com/fredericksgary/z"
   :license {:name "Eclipse Public License"
